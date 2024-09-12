95a0561cc2479f45840f33cb32c68783a3d89237
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2024-09-12 15:10:20 -0300

    Update release.sh

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -30,22 +30,19 @@ fi
 VERSION=$(echo $TAG | sed -r -e 's/BABEL_([0-9a-z_]*)__PG.*/\1/' -e 's/_/./g')
 ENGINE=$(echo $TAG | sed -r -e 's/BABEL_([0-9_]*)__PG_([0-9]+_)/\2/' -e 's/_/./g')
 
-title="Babelfish ${VERSION} for PostgreSQL ${ENGINE} - ${RELEASE_VERSION}"
-
 # change babelfish version format to major-minor-patch
 VERSION=$(echo $TAG | sed -r -e 's/BABEL_([0-9a-z_]*)__PG.*/\1/' -e 's/_/-/g')
 
-release=$title". The release notes are [here](https://babelfishpg.org/docs/versions/babelfish-${VERSION}.html)."
-
 
 NIGHTLY_VERSION=$RELEASE_VERSION-$(date '+%Y-%m-%d')
+
+title="Babelfish ${NIGHTLY_VERSION}"
 
 echo "
 Releasing
 Tag: $NIGHTLY_VERSION
 Title: $title
 Attachments: ${LOC}${TAG}.*
-Release Notes: $release
 "
 
-gh release create --draft $NIGHTLY_VERSION ${LOC}${TAG}.* -d -t "$title" -n "$release"
+gh release create --draft $NIGHTLY_VERSION ${LOC}${TAG}.* -d -t "$title" -n ""
